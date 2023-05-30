af320809b38acd7f23649159a1b54957cd08f641
mfelipperd <felipperabelodurans@gmail.com>
2023-05-30 19:17:00 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['https://form.expomultimix.com', 'http://localhost:3000'],
+    origin: ['https://form.expomultimix.com'], //'http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     // credentials: true,
   });
